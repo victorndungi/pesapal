de471501dd92a1e341bbebde73b4bc864911c623
victorndungi <victor.ndungi@student.moringaschool.com>
2024-11-19 13:25:03 +0300

    added ipn registration

--- app.py
+++ app.py
@@ -24,6 +24,36 @@ def get_pesapaltoken():
         return jsonify(response.json()), response.status_code
     except requests.exceptions.RequestException as e:
         return jsonify({"error": str(e)}), 500
+    
+@app.route('/registerIPNURL', methods=['POST'])
+def register_ipn():
+    ipn_url = f"{base_url}/api/URLSetup/RegisterIPN"
+
+    data = request.get_json()
+    session_token = data.get('sessionToken')
+    #redirect url: callback url
+    redirect_url = "https://friendly-nougat-e4e413.netlify.app/" #simple hosted html page
+
+    ipn_request_payload = {
+        "url" : redirect_url,
+        "ipn_notification_type": "GET"
+    }
+
+    try:
+        headers = {
+            "Content-type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Bearer {session_token}"
+        }
+        response = requests.post(ipn_url, json = ipn_request_payload, headers=headers)
+
+        response.raise_for_status()
+        return jsonify(response.json()), response.status_code
+
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(port=5000)
